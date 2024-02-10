e08951723d73486449b602f84f6bee9fc7a7549d
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2024-02-10 23:02:50 +1300

    feat: add the rest of task solutions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -566,8 +566,9 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const newArr = arr1.filter((item) => arr2.includes(item) === true);
+  return newArr;
 }
 
 /**
@@ -581,8 +582,22 @@ function findCommonElements(/* arr1, arr2 */) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let counter = 1;
+  const accum = [];
+  const totalCounter = nums.reduce((n, item, index, array) => {
+    if (index !== 0) {
+      if (array[index] > array[index - 1]) {
+        counter += 1;
+        accum.push(counter);
+      } else {
+        accum.push(counter);
+        counter = 1;
+      }
+    }
+    return accum;
+  });
+  return totalCounter.reduce((max, item) => Math.max(max, item), 0);
 }
 
 /**
@@ -599,8 +614,17 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+  const newArr = arr.map((item, index) => {
+    const itemArr = [];
+    itemArr.length = index + 1;
+    itemArr.fill(item);
+    return itemArr;
+  });
+  return newArr.flat();
 }
 
 /**
@@ -616,8 +640,17 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  if (n >= 0) {
+    return arr.map((item, index, array) =>
+      index - n >= 0 ? array[index - n] : array[index - n + array.length]
+    );
+  }
+  return arr.map((item, index, array) =>
+    index - n < array.length
+      ? array[index - n]
+      : array[index - n - array.length]
+  );
 }
 
 /**
@@ -633,8 +666,25 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const digitNamesArr = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  const newArr = arr.sort((a, b) => {
+    const aIndex = digitNamesArr.indexOf(a);
+    const bIndex = digitNamesArr.indexOf(b);
+    return aIndex - bIndex;
+  });
+  return newArr;
 }
 
 /**
@@ -656,8 +706,14 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length === 0 || arr.length === 1) {
+    return arr;
+  }
+  const n = Math.floor(arr.length / 2);
+  const head = arr.slice(0, n);
+  const tail = arr.slice(-n);
+  return arr.length % 2 === 0 ? tail.concat(head) : tail.concat(arr[n], head);
 }
 
 module.exports = {
